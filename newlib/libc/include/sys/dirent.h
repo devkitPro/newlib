33d38359696e8c5e650bb62cd41d90de8f707ab6
Dave Murphy <davem@devkitpro.org>
2018-06-07 01:24:12 +0100

    add scandir
    (cherry picked from commit db6327328cc924fae5274a611b44d2225c98cdd1)

--- newlib/libc/include/sys/dirent.h
+++ newlib/libc/include/sys/dirent.h
@@ -26,19 +26,19 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-	
+
 	struct dirent {
 		ino_t	d_ino;
 		unsigned char  d_type;
 		char	d_name[NAME_MAX+1];
 	};
-	
+
 	typedef struct {
 		long int        position;
 		DIR_ITER*       dirData;
 		struct dirent   fileData;
 	} DIR;
-	
+
 	int closedir(DIR *dirp);
 	DIR *opendir(const char *dirname);
 	struct dirent *readdir(DIR *dirp);
@@ -46,7 +46,13 @@ extern "C" {
 	void rewinddir(DIR *dirp);
 	void seekdir(DIR *dirp, long int loc);
 	long int telldir(DIR *dirp);
-	
+
+	int scandir(const char *dirp, struct dirent ***namelist,
+		int (*filter)(const struct dirent *),
+		int (*compar)(const struct dirent **, const struct dirent **));
+
+	int alphasort(const struct dirent **a, const struct dirent **b);
+
 #ifdef __cplusplus
 }
 #endif

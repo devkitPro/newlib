890c2da8d82d2f20a23aba5d8bc454d1b194c658
fincs <fincs@devkitpro.org>
2019-03-26 20:27:49 +0100

    Implement pthread barrier, rwlock & posix semaphore

--- libgloss/libsysbase/pthread.c
+++ libgloss/libsysbase/pthread.c
@@ -1,9 +1,16 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 #include <sys/iosupport.h>
+
+static inline uint64_t
+__pthread_abstime_to_ns (const struct timespec *__abstime)
+{
+	return __abstime ? abstimespec2nsec(CLOCK_REALTIME, __abstime) : UINT64_MAX;
+}
 
 int
 pthread_atfork (void (*prepare)(void), void (*parent)(void), void (*child)(void))
@@ -82,10 +89,10 @@ pthread_mutex_init (pthread_mutex_t *__mutex, const pthread_mutexattr_t *__attr)
 		case PTHREAD_MUTEX_NORMAL:
 		case PTHREAD_MUTEX_ERRORCHECK:
 		case PTHREAD_MUTEX_DEFAULT:
-			__lock_init(__mutex->normal);
+			__mutex->normal = __LOCK_INITIALIZER;
 			break;
 		case PTHREAD_MUTEX_RECURSIVE:
-			__lock_init_recursive(__mutex->recursive);
+			__mutex->recursive = __LOCK_INITIALIZER_RECURSIVE;
 			break;
 		default:
 			return EINVAL;
@@ -238,7 +245,8 @@ pthread_cond_init (pthread_cond_t *__cond, const pthread_condattr_t *__attr)
 		return EINVAL;
 
 	__cond->clock_id = __attr->clock_id;
-	return __cond_init(__cond->cond);
+	__cond->cond = __COND_INITIALIZER;
+	return 0;
 }
 
 int
@@ -641,6 +649,419 @@ _pthread_cleanup_pop (struct _pthread_cleanup_context *_context, int _execute)
 			cur->_routine(cur->_arg);
 		pthread_setspecific(__pthread_cleanup_key, cur->_previous);
 	}
+}
+
+//-----------------------------------------------------------------------------
+// Barrier
+//-----------------------------------------------------------------------------
+
+int
+pthread_barrierattr_init (pthread_barrierattr_t *__attr)
+{
+	if (!__attr)
+		return EINVAL;
+	return 0;
+}
+
+int
+pthread_barrierattr_destroy (pthread_barrierattr_t *__attr)
+{
+	if (!__attr)
+		return EINVAL;
+	return 0;
+}
+
+int
+pthread_barrierattr_getpshared (const pthread_barrierattr_t *__attr, int *__pshared)
+{
+	return ENOSYS;
+}
+
+int
+pthread_barrierattr_setpshared (pthread_barrierattr_t *__attr, int __pshared)
+{
+	return ENOSYS;
+}
+
+int
+pthread_barrier_init (pthread_barrier_t *__barrier, const pthread_barrierattr_t *__attr, unsigned __count)
+{
+	if (!__barrier || !__count)
+		return EINVAL;
+
+	__barrier->lock = __LOCK_INITIALIZER;
+	__barrier->cond = __COND_INITIALIZER;
+	__barrier->reload = __count;
+	__barrier->counter = __count;
+	__barrier->cycle = 0;
+	return 0;
+}
+
+int
+pthread_barrier_destroy (pthread_barrier_t *__barrier)
+{
+	if (!__barrier)
+		return EINVAL;
+	return 0;
+}
+
+int
+pthread_barrier_wait (pthread_barrier_t *__barrier)
+{
+	if (!__barrier)
+		return EINVAL;
+
+	__lock_acquire(__barrier->lock);
+
+	int is_last_thread = !--__barrier->counter;
+	if (is_last_thread) {
+		__barrier->cycle ++;
+		__barrier->counter = __barrier->reload;
+		__cond_broadcast(__barrier->cond);
+	} else {
+		unsigned my_cycle = __barrier->cycle;
+		do
+			__cond_wait(__barrier->cond, __barrier->lock, UINT64_MAX);
+		while (__barrier->cycle == my_cycle);
+	}
+
+	__lock_release(__barrier->lock);
+	return is_last_thread ? PTHREAD_BARRIER_SERIAL_THREAD : 0;
+}
+
+//-----------------------------------------------------------------------------
+// Read/write lock
+//-----------------------------------------------------------------------------
+
+int
+pthread_rwlockattr_init (pthread_rwlockattr_t *__attr)
+{
+	if (!__attr)
+		return EINVAL;
+	return 0;
+}
+
+int
+pthread_rwlockattr_destroy (pthread_rwlockattr_t *__attr)
+{
+	if (!__attr)
+		return EINVAL;
+	return 0;
+}
+
+int
+pthread_rwlockattr_getpshared (const pthread_rwlockattr_t *__attr, int *__pshared)
+{
+	return ENOSYS;
+}
+
+int
+pthread_rwlockattr_setpshared (pthread_rwlockattr_t *__attr, int __pshared)
+{
+	return ENOSYS;
+}
+
+int
+pthread_rwlock_init (pthread_rwlock_t *__rwlock, const pthread_rwlockattr_t *__attr)
+{
+	if (!__rwlock)
+		return EINVAL;
+	*__rwlock = PTHREAD_RWLOCK_INITIALIZER;
+	return 0;
+}
+
+int
+pthread_rwlock_destroy (pthread_rwlock_t *__rwlock)
+{
+	if (!__rwlock)
+		return EINVAL;
+	return 0;
+}
+
+static int
+__pthread_rwlock_rdlock_common (pthread_rwlock_t *__rwlock, const struct timespec *__abstime)
+{
+	int rc = 0;
+	__lock_acquire(__rwlock->lock);
+
+	while (__rwlock->cnt_w) {
+		rc = __cond_wait(__rwlock->cond_w, __rwlock->lock, __pthread_abstime_to_ns(__abstime));
+		if (rc) break;
+	}
+
+	if (!rc)
+		__rwlock->cnt_r ++;
+
+	__lock_release(__rwlock->lock);
+	return rc;
+}
+
+int
+pthread_rwlock_rdlock (pthread_rwlock_t *__rwlock)
+{
+	if (!__rwlock)
+		return EINVAL;
+	return __pthread_rwlock_rdlock_common(__rwlock, NULL);
+}
+
+int
+pthread_rwlock_tryrdlock (pthread_rwlock_t *__rwlock)
+{
+	if (!__rwlock)
+		return EINVAL;
+
+	__lock_acquire(__rwlock->lock);
+
+	int failed = __rwlock->cnt_w != 0;
+	if (!failed)
+		__rwlock->cnt_r ++;
+
+	__lock_release(__rwlock->lock);
+	return failed ? EBUSY : 0;
+}
+
+int
+pthread_rwlock_timedrdlock (pthread_rwlock_t *__rwlock, const struct timespec *__abstime)
+{
+	if (!__rwlock || !__abstime)
+		return EINVAL;
+	return __pthread_rwlock_rdlock_common(__rwlock, __abstime);
+}
+
+int
+pthread_rwlock_unlock (pthread_rwlock_t *__rwlock)
+{
+	if (!__rwlock)
+		return EINVAL;
+
+	__lock_acquire(__rwlock->lock);
+
+	if (__rwlock->cnt_w != 2) {
+		if (!--__rwlock->cnt_r)
+			__cond_broadcast(__rwlock->cond_r);
+	} else {
+		__rwlock->cnt_w = 0;
+		__cond_broadcast(__rwlock->cond_w);
+	}
+
+	__lock_release(__rwlock->lock);
+	return 0;
+}
+
+static int
+__pthread_rwlock_wrlock_common (pthread_rwlock_t *__rwlock, const struct timespec *__abstime)
+{
+	int rc = 0;
+	__lock_acquire(__rwlock->lock);
+
+	while (__rwlock->cnt_w) {
+		rc = __cond_wait(__rwlock->cond_w, __rwlock->lock, __pthread_abstime_to_ns(__abstime));
+		if (rc) break;
+	}
+
+	if (!rc) {
+		__rwlock->cnt_w = 1;
+
+		while (__rwlock->cnt_r) {
+			rc = __cond_wait(__rwlock->cond_r, __rwlock->lock, __pthread_abstime_to_ns(__abstime));
+			if (rc) break;
+		}
+
+		if (rc) {
+			__rwlock->cnt_w = 0;
+			__cond_broadcast(__rwlock->cond_w);
+		} else
+			__rwlock->cnt_w = 2;
+	}
+
+	__lock_release(__rwlock->lock);
+	return rc;
+}
+
+int
+pthread_rwlock_wrlock (pthread_rwlock_t *__rwlock)
+{
+	if (!__rwlock)
+		return EINVAL;
+	return __pthread_rwlock_wrlock_common(__rwlock, NULL);
+}
+
+int
+pthread_rwlock_trywrlock (pthread_rwlock_t *__rwlock)
+{
+	if (!__rwlock)
+		return EINVAL;
+
+	__lock_acquire(__rwlock->lock);
+
+	int failed = __rwlock->cnt_w != 0 || __rwlock->cnt_r != 0;
+	if (!failed)
+		__rwlock->cnt_w = 2;
+
+	__lock_release(__rwlock->lock);
+	return failed ? EBUSY : 0;
+}
+
+int
+pthread_rwlock_timedwrlock (pthread_rwlock_t *__rwlock, const struct timespec *__abstime)
+{
+	if (!__rwlock || !__abstime)
+		return EINVAL;
+	return __pthread_rwlock_wrlock_common(__rwlock, __abstime);
+}
+
+//-----------------------------------------------------------------------------
+// semaphore.h
+//-----------------------------------------------------------------------------
+
+int
+sem_close(sem_t *__sem)
+{
+	errno = ENOSYS;
+	return -1;
+}
+
+int
+sem_destroy(sem_t *__sem)
+{
+	if (!__sem) {
+		errno = EINVAL;
+		return -1;
+	}
+	return 0;
+}
+
+int
+sem_getvalue(sem_t *__sem, int *__sval)
+{
+	if (!__sem || !__sval) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	__lock_acquire(__sem->lock);
+	*__sval = __sem->value;
+	__lock_release(__sem->lock);
+	return 0;
+}
+
+int
+sem_init(sem_t *__sem, int __pshared, unsigned int __value)
+{
+	if (!__sem || __value > SEM_VALUE_MAX) {
+		errno = EINVAL;
+		return -1;
+	}
+	if (__pshared) {
+		errno = ENOSYS;
+		return -1;
+	}
+
+	__sem->lock = __LOCK_INITIALIZER;
+	__sem->cond = __COND_INITIALIZER;
+	__sem->value = __value;
+	return 0;
+}
+
+sem_t *
+sem_open(const char *__name, int __oflag, ...)
+{
+	errno = ENOSYS;
+	return SEM_FAILED;
+}
+
+int
+sem_post(sem_t *__sem)
+{
+	if (!__sem) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	__lock_acquire(__sem->lock);
+	if (__sem->value++ < 0)
+		__cond_signal(__sem->cond);
+	__lock_release(__sem->lock);
+	return 0;
+}
+
+static int
+__sem_wait_common(sem_t *__sem, const struct timespec *__abstime)
+{
+	int rc = 0;
+	__lock_acquire(__sem->lock);
+
+	__sem->value --;
+	if (__sem->value < 0)
+		rc = __cond_wait(__sem->cond, __sem->lock, __pthread_abstime_to_ns(__abstime));
+	if (rc) {
+		if (__sem->value < 0)
+			__sem->value ++;
+		else
+			rc = 0;
+	}
+
+	__lock_release(__sem->lock);
+
+	if (rc) {
+		errno = rc;
+		return -1;
+	}
+
+	return 0;
+}
+
+int
+sem_timedwait(sem_t *__sem, const struct timespec *__abstime)
+{
+	if (!__sem || !__abstime) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	return __sem_wait_common(__sem, __abstime);
+}
+
+int
+sem_trywait(sem_t *__sem)
+{
+	if (!__sem) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	__lock_acquire(__sem->lock);
+
+	int failed = __sem->value <= 0;
+	if (!failed)
+		__sem->value --;
+
+	__lock_release(__sem->lock);
+
+	if (failed) {
+		errno = EAGAIN;
+		return -1;
+	}
+
+	return 0;
+}
+
+int
+sem_unlink(const char *__name)
+{
+	errno = ENOSYS;
+	return -1;
+}
+
+int
+sem_wait(sem_t *__sem)
+{
+	if (!__sem) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	return __sem_wait_common(__sem, NULL);
 }
 
 //-----------------------------------------------------------------------------

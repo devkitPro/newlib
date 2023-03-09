40f2cc97bb6ab9fd8a62ee7c9bdd18377340702a
Dave Murphy <davem@devkitpro.org>
2023-03-09 23:30:10 +0000

    add deviceData

--- newlib/libc/include/sys/reent.h
+++ newlib/libc/include/sys/reent.h
@@ -419,6 +419,8 @@ struct _reent
   __FILE *__sf;			        /* file descriptors */
   struct _misc_reent *_misc;            /* strtok, multibyte states */
   char *_signal_buf;                    /* strsignal */
+
+  void *deviceData;
 };
 
 # define _REENT_INIT(var) \
@@ -639,6 +641,8 @@ struct _reent
 
   /* signal info */
   void (**_sig_func)(int);
+
+  void *deviceData;
 };
 
 #define _REENT_INIT(var) \
